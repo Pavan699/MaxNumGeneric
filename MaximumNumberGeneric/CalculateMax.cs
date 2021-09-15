0a94e6fbca92dd93b2539ddbefa19b3dcf37b210
Pavan699 <pavannakate03@gmail.com>
2021-09-15 06:43:38 -0700

    Program By Using Generic Class

--- MaximumNumberGeneric/CalculateMax.cs
+++ MaximumNumberGeneric/CalculateMax.cs
@@ -3,25 +3,33 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace MaximumNumberGeneric
-{
-    class CalculateMax
+{   
+    class CalculateMax<classtype>//create the "classtype" for the class
     {
-        public static dynamic Check<datatype>(dynamic num1 , dynamic num2 , dynamic num3)
+        dynamic num1;//class intances to store the data
+        dynamic num2;
+        dynamic num3;
+        public CalculateMax(classtype numOne, classtype numTwo, classtype numThree)//constructor creation
+        {
+            this.num1 = numOne;//assining the values
+            this.num2 = numTwo;
+            this.num3 = numThree;
+        }          
+        public void Print()//print method to print the maximum values for Integer,Float and String
         {
             if(num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
-            {               
-                return num1;
+            {
+                Console.WriteLine("Maximum Value is : " + num1);
             }
             if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
-            {                
-                return num2;
+            {
+                Console.WriteLine("Maximum Value is : " + num2);
             }
             if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
             {
-                return num3;
+                Console.WriteLine("Maximum Value is : " + num3);
             }
-            Console.WriteLine(num1);
-            return num1;
-        }     
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++");
+        }           
     }
 }
--- MaximumNumberGeneric/Program.cs
+++ MaximumNumberGeneric/Program.cs
@@ -6,14 +6,12 @@ namespace MaximumNumberGeneric
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Maximum Number Program");
-            int[] numbers = {};
-            int MaxIntNum = CalculateMax.Check<int>(79, 156, 31);
-            Console.WriteLine("Maximum Integer Number is : {0}",MaxIntNum);
-            float MaxFloatNum = CalculateMax.Check<float>(3.8f, 1.8f, 9.4f);
-            Console.WriteLine("Maximum Float Number is : {0}", MaxFloatNum);
-            string MaxStringNum = CalculateMax.Check<string>("Apple","Peach","Banana");
-            Console.WriteLine("Maximum Float Number is : {0}", MaxStringNum);
+            Console.WriteLine("Maximum Number Program By Using Generic Class");
+            Console.WriteLine("Maximum Value For Integer,Float and String :");
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++");
+            new CalculateMax<int>(35,156,91).Print();            
+            new CalculateMax<float>(3.8f, 1.8f, 9.4f).Print();           
+            new CalculateMax<string>("Apple","Peach","Banana").Print();           
         }
     }
 }
