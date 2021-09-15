922791084ae77fd98d844cb7f9c171ca06904a43
Pavan699 <pavannakate03@gmail.com>
2021-09-15 04:41:48 -0700

    Adding String Max Logic

--- MaximumNumberGeneric/CalculateMax.cs
+++ MaximumNumberGeneric/CalculateMax.cs
@@ -38,5 +38,21 @@ namespace MaximumNumberGeneric
             }
             return num1;
         }
+        public static string Check(string num1, string num2, string num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+            {
+                return num1;
+            }
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
+            {
+                return num2;
+            }
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
+            {
+                return num3;
+            }
+            return num1;
+        }
     }
 }
--- MaximumNumberGeneric/Program.cs
+++ MaximumNumberGeneric/Program.cs
@@ -11,6 +11,8 @@ namespace MaximumNumberGeneric
             Console.WriteLine("Maximum Integer Number is : {0}",MaxIntNum);
             float MaxFloatNum = CalculateMax.Check(3.8f, 1.8f, 9.4f);
             Console.WriteLine("Maximum Float Number is : {0}", MaxFloatNum);
+            string MaxStringNum = CalculateMax.Check("Apple","Peach","Banana");
+            Console.WriteLine("Maximum Float Number is : {0}", MaxStringNum);
         }
     }
 }
