746ca86ac2e2f62a413fe270f34b5cd8fe3fd3d4
Pavan699 <pavannakate03@gmail.com>
2021-09-15 04:22:22 -0700

    Adding Max Float Number logic

--- MaximumNumberGeneric/CalculateMax.cs
+++ MaximumNumberGeneric/CalculateMax.cs
@@ -22,5 +22,21 @@ namespace MaximumNumberGeneric
             }
             return num1;
         }
+        public static float Check(float num1, float num2, float num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+            {
+                return num1;
+            }
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
+            {
+                return num2;
+            }
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
+            {
+                return num3;
+            }
+            return num1;
+        }
     }
 }
--- MaximumNumberGeneric/Program.cs
+++ MaximumNumberGeneric/Program.cs
@@ -7,8 +7,10 @@ namespace MaximumNumberGeneric
         static void Main(string[] args)
         {
             Console.WriteLine("Maximum Number Program");
-            int MaxNum = CalculateMax.Check(79, 156, 31);
-            Console.WriteLine("Maximum Nuber is : {0}",MaxNum);
+            int MaxIntNum = CalculateMax.Check(79, 156, 31);
+            Console.WriteLine("Maximum Integer Number is : {0}",MaxIntNum);
+            float MaxFloatNum = CalculateMax.Check(3.8f, 1.8f, 9.4f);
+            Console.WriteLine("Maximum Float Number is : {0}", MaxFloatNum);
         }
     }
 }
