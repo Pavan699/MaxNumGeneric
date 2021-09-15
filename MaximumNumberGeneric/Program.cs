8f894942a65588f5f1f7b6fcefccf562dc053fb9
Pavan699 <pavannakate03@gmail.com>
2021-09-15 04:17:39 -0700

    Adding Max Int Number

---
+++ MaximumNumberGeneric/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MaximumNumberGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maximum Number Program");
+            int MaxNum = CalculateMax.Check(79, 156, 31);
+            Console.WriteLine("Maximum Nuber is : {0}",MaxNum);
+        }
+    }
+}
