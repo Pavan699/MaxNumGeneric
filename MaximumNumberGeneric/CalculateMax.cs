f15342908c9bbbacb4553dc3104631a97ce0aa19
Pavan699 <pavannakate03@gmail.com>
2021-09-20 19:26:19 -0700

    Passing /// Comments to Program

--- MaximumNumberGeneric/CalculateMax.cs
+++ MaximumNumberGeneric/CalculateMax.cs
@@ -6,6 +6,13 @@ namespace MaximumNumberGeneric
 {
     class CalculateMax
     {
+        /// <summary>
+        /// Check() method to compare Integer numbers
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="num3"></param>
+        /// <returns></returns>
         public static int Check(int num1,int num2,int num3)
         {
             if(num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
@@ -22,6 +29,13 @@ namespace MaximumNumberGeneric
             }
             return num1;
         }
+        /// <summary>
+        /// Check() method to compare Floating numbers
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="num3"></param>
+        /// <returns></returns>
         public static float Check(float num1, float num2, float num3)
         {
             if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
